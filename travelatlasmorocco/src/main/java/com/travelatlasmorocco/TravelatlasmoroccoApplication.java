c4c6deb5a485be97eadbd5225eaa9ea0781524a1
ilias alilou <ilias.alilou@hps-worldwide.com>
2023-06-21 11:54:13 +0100

    initial project

---
+++ travelatlasmorocco/src/main/java/com/travelatlasmorocco/TravelatlasmoroccoApplication.java
@@ -0,0 +1,13 @@
+package com.travelatlasmorocco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelatlasmoroccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelatlasmoroccoApplication.class, args);
+    }
+
+}
---
+++ travelatlasmorocco/src/test/java/com/travelatlasmorocco/TravelatlasmoroccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelatlasmorocco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelatlasmoroccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
