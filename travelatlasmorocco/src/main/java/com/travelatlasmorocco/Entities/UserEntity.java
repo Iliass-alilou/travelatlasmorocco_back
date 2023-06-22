0545876ced7299087fc65a0e9295d36079955075
ilias alilou <ilias.alilou@hps-worldwide.com>
2023-06-22 15:03:04 +0100

    update dependencies

--- travelatlasmorocco/src/main/java/com/travelatlasmorocco/Entities/UserEntity.java
+++ travelatlasmorocco/src/main/java/com/travelatlasmorocco/Entities/UserEntity.java
@@ -1,7 +1,13 @@
 package com.travelatlasmorocco.Entities;
-
+import javax.persistence.Id;
 import javax.persistence.Entity;
+
 @Entity(name = "users")
 public class UserEntity {
-
+    @Id
+    private long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private Boolean admin = false;
 }
